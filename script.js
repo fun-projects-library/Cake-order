cc841792d9748393590a1fd98d683896b330648f
fehmi-enes-eren <76635233+fehmi-enes-eren@users.noreply.github.com>
2021-03-10 18:13:18 -0700

    createFolder

---
+++ script.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const patisserie = {
+  bananaCaramel: {
+    stock: 3,
+    price: 9.99,
+  },
+  contessa: {
+    stock: 5,
+    price: 7.99,
+  },
+  concorde: {
+    stock: 11,
+    price: 22.99,
+  },
+  mouseCake: {
+    stock: 8,
+    price: 16.99,
+  },
+  confettiSuprise: {
+    stock: 9,
+    price: 14.99,
+  },
+};
+
+
+
+const checkOrder = (order) => {
+  
+  // dont forget setTimeout
+
+
+};
+
+
+
+const payment = (resolvedValue) => {
+  
+  // dont forget setTimeout
+  
+}
+
+const checkStock = (resolvedValue) => {
+  
+  // dont forget setTimeout
+
+  }
+
+
+
+orderBtn.onclick = ()=>{
+  // let order = ['contessa', 2];   // sample order template, you should take values from DOM
+  // create promise chain
+
+ 
+
+}
